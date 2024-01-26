111d6f20c07139f7bb9e648c78b935e11322945f
darya-strekalovskaya <darya.strek@gmail.com>
2024-01-27 02:16:21 +0300

    Application.java

--- app/src/main/java/hexlet/code/Application.java
+++ app/src/main/java/hexlet/code/Application.java
@@ -13,17 +13,34 @@ public class Application {
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var statement2 = conn.prepareStatement(sql2)) {
+                statement2.setString(1, "Sarah");
+                statement2.setString(2, "931-999-99-99");
+                statement2.executeUpdate();
+
+                statement2.setString(1, "Piter");
+                statement2.setString(2, "931-999-88-88");
+                statement2.executeUpdate();
+
+                statement2.setString(1, "Morgan");
+                statement2.setString(2, "931-999-77-77");
+                statement2.executeUpdate();
             }
 
-            var sql3 = "SELECT * FROM users";
-            try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
+            var sql3 = "DELETE FROM users WHERE username = ?";
+            try (var statement3 = conn.prepareStatement(sql3)) {
+                statement3.setString(1, "Morgan");
+                statement3.execute();
+            }
+
+            var sql4 = "SELECT * FROM users";
+            try (var statement4 = conn.createStatement()) {
+                var resultSet = statement4.executeQuery(sql4);
                 while (resultSet.next()) {
-                    System.out.println(resultSet.getString("username"));
-                    System.out.println(resultSet.getString("phone"));
+                    System.out.printf("%s %s\n",
+                                        resultSet.getString("username"),
+                                        resultSet.getString("phone"));
                 }
             }
         }
